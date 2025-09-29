da08fb0094a235e67f65dc7cdee178c906219d5e
kanitvural <kanitvural@gmail.com>
2025-09-29 19:58:04 +0300

    CORS configurations added to lambda

--- RestaurantProject/lambda_handler.py
+++ RestaurantProject/lambda_handler.py
@@ -2,6 +2,18 @@ import boto3
 import json
 
 def lambda_handler(event, context):
+    # Preflight request (OPTIONS) için cevap
+    if event.get("httpMethod") == "OPTIONS":
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            "body": ""
+        }
+
     try:
         body = json.loads(event.get("body", "{}"))
         prompt = body.get("prompt")
@@ -11,16 +23,17 @@ def lambda_handler(event, context):
             return {
                 "statusCode": 400,
                 "body": json.dumps({"message": "Both 'prompt' and 'sessionId' are required."}),
-                "headers": {"Content-Type": "application/json"}
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                }
             }
 
         client = boto3.client('bedrock-agentcore', region_name='eu-central-1')
-        payload = json.dumps({
-            "prompt": prompt
-        })
+        payload = json.dumps({"prompt": prompt})
 
         response = client.invoke_agent_runtime(
-            agentRuntimeArn='arn:aws:bedrock-agentcore:eu-central-1:058264126563:runtime/multi_agent_restaurant-ctzDy45ULO',
+            agentRuntimeArn='arn:aws:bedrock-agentcore:eu-central-1:058264126563:runtime/multi_agent_restaurant-wg3fXH8MuC',
             runtimeSessionId=session_id,
             payload=payload,
             qualifier="DEFAULT"
@@ -32,15 +45,22 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "body": json.dumps({"response": response_data.get("result")}),
-            "headers": {"Content-Type": "application/json"}
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
         }
 
     except Exception as e:
         return {
             "statusCode": 500,
             "body": json.dumps({"message": str(e)}),
-            "headers": {"Content-Type": "application/json"}
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
         }
+
         
 # Postman test
 
